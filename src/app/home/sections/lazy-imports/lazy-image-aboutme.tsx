4cc616d501ff4201651f9e227c4b0a223f2db201
Otochicatole <164841448+Otochicatole@users.noreply.github.com>
2025-05-16 00:35:12 -0300

    fix problem build

--- src/app/home/sections/lazy-imports/lazy-image-aboutme.tsx
+++ src/app/home/sections/lazy-imports/lazy-image-aboutme.tsx
@@ -1,12 +1,14 @@
 import { memo } from "react";
+import Image from "next/image";
 
 type Props = {
     src: string;
     className?: string;
+    alt?: string; // Agregar alt para accesibilidad
 };
 
-const LazyImage = memo(({ src, className }: Props) => (
-    <img className={className} src={src} alt="" loading="lazy" />
+const LazyImage = memo(({ src, className, alt = "" }: Props) => (
+    <Image className={className} src={src} alt={alt} loading="lazy" />
 ));
 
 LazyImage.displayName = "LazyImage";
--- src/app/home/text/about-me-text.tsx
+++ src/app/home/text/about-me-text.tsx
@@ -1,13 +1,13 @@
 export const text = {
-    imgText: <p className="text-[15px]  text-[#545454] h-full">Ciao, sono Manuela Maranzana, una fotografa appassionata nel catturare l'essenza di ogni storia attraverso uno stile moderno, minimalista e molto emotivo. <br /><br />
+    imgText: <p className="text-[15px]  text-[#545454] h-full">Ciao, sono Manuela Maranzana, una fotografa appassionata nel catturare l&apos;essenza di ogni storia attraverso uno stile moderno, minimalista e molto emotivo. <br /><br />
         Credo nella bellezza della semplicità e in come i piccoli dettagli possano raccontare le emozioni più profonde. <br /><br />
-        Per me, ogni foto è un'opportunità per immortalare momenti unici, da quegli sguardi pieni di complicità alle risate spontanee che riflettono una felicità genuina. <br /><br />
+        Per me, ogni foto è un&apos;opportunità per immortalare momenti unici, da quegli sguardi pieni di complicità alle risate spontanee che riflettono una felicità genuina. <br /><br />
         Mi piace ritrarre la personalità di ogni coppia e trasformare i loro momenti speciali in ricordi che durano per sempre. <br /><br />
         Lavoro con amore e passione, dedicandomi a creare immagini che non solo siano belle, ma che trasmettano anche tutto ciò che hanno sentito in quel giorno così speciale. <br /><br />
         Se cerchi fotografie che catturino le tue emozioni in modo autentico e senza tempo, sarò felice di aiutarti a raccontare la tua storia!</p>,
 
     missioneText: <p className="lg:text-[18px]  text-[#545454]">Catturare i momenti più emozionanti delle persone nel loro giorno speciale, riflettendo la personalità unica di ogni coppia e creando ricordi che possano essere rivissuti con la stessa emozione attraverso le nostre fotografie.</p>,
-    visioneText: <p className="lg:text-[18px]  text-[#545454]">Essere riconosciuti come il punto di riferimento nella fotografia di eventi in Italia, distinguendoci per la nostra capacità di immortalare le emozioni più autentiche e ritrarre l'essenza unica di ogni coppia, creando esperienze indimenticabili per i nostri clienti.</p>,
+    visioneText: <p className="lg:text-[18px]  text-[#545454]">Essere riconosciuti come il punto di riferimento nella fotografia di eventi in Italia, distinguendoci per la nostra capacità di immortalare le emozioni più autentiche e ritrarre l&apos;essenza unica di ogni coppia, creando esperienze indimenticabili per i nostri clienti.</p>,
     valoriText: <p className="lg:text-[17px]  text-[#545454]"><span className="font-semibold mr-1">Emozione e autenticità:</span> Mi concentro nel catturare i sentimenti reali di ogni momento. <br /><br />
 
         <span className="font-semibold mr-1">Cura dei dettagli:</span> Ogni fotografia riflette la mia dedizione e passione per la perfezione. <br /><br />
@@ -18,7 +18,7 @@ export const text = {
 
         <span className="font-semibold mr-1">Personalizzazione:</span> Adatto il mio approccio per valorizzare ciò che rende speciale ogni coppia. <br /><br />
 
-        I<span className="font-semibold mr-1">nnovazione:</span> Esploro nuove tecniche e stili per offrire un prodotto unico ed eccezionale.</p>,
+        <span className="font-semibold mr-1">Innovazione:</span> Esploro nuove tecniche e stili per offrire un prodotto unico ed eccezionale.</p>,
     parcheIoText: <div className="max-w-2xl mx-auto text-[#545454] text-[15px] leading-relaxed">
         <p>
             Perché i momenti speciali non si ripetono, ma possono essere rivissuti ancora e ancora con le fotografie giuste.
@@ -35,7 +35,7 @@ export const text = {
         </ul>
 
         <p className="mt-4">
-            Non avrai solo immagini belle; avrai un'eredità emotiva da condividere con le persone che ami di più.
+            Non avrai solo immagini belle; avrai un&apos;eredità emotiva da condividere con le persone che ami di più.
         </p>
 
         <p className="mt-4">
