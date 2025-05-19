1158a765bdcd5aa7cb9cde859dc44cd8d0bc434a
Otochicatole <164841448+Otochicatole@users.noreply.github.com>
2025-05-19 13:23:34 -0300

    icon / title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import "./globals.css";
 import React from "react";
 
+export const metadata = {
+  title: "Manu Maranzana",
+};
+
 export default function RootLayout({
                                        children,
                                    }: Readonly<{
@@ -8,6 +12,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
+        <head>
+            <link rel="icon" href="/logo.svg" sizes="any" />
+        </head>
         <body className="flex flex-col min-h-screen">
         {children}
         </body>
